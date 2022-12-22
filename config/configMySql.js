74059a20ab4274e89ced79363c1d85a89c6072ef
Julieta <juli.mulinaris@gmail.com>
2022-12-22 16:09:45 -0300

    modifico sql

--- config/configMySql.js
+++ config/configMySql.js
@@ -1,11 +1,11 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const DB_CLIENT = process.env.DB_CLIENT_SQL;
-const DB_HOST = process.env.DB_HOST_SQL;
-const DB_PORT = Number(process.env.DB_PORT_SQL);
-const DB_USER = process.env.DB_USER_SQL;
-const DB_NAME = process.env.DB_NAME_SQL;
+const DB_CLIENT = process.env.DB_CLIENT_SQL || "mysql"
+const DB_HOST = process.env.DB_HOST_SQL || "localhost";
+const DB_PORT = Number(process.env.DB_PORT_SQL) || 3306
+const DB_USER = process.env.DB_USER_SQL || "root"
+const DB_NAME = process.env.DB_NAME_SQL || "bd_productos"
 
 const connection = {
     client: `${DB_CLIENT}`,
